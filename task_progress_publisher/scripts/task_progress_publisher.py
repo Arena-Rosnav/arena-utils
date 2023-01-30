daf9243c1ba7e2451ba69c008f4dcb37a1a668ea
ReykCS <reyk-carstens@web.de>
2023-01-30 11:40:28 +0100

    added new best model endpoint handler to task progress publisher

--- task_progress_publisher/scripts/task_progress_publisher.py
+++ task_progress_publisher/scripts/task_progress_publisher.py
@@ -1,4 +1,5 @@
 import rospy
+import os
 
 import requests
 from std_msgs.msg import Empty, Bool
@@ -10,16 +11,29 @@ class TaskProgressPublisher:
         self.task_id = rospy.get_param("task_id")
         self.app_token = rospy.get_param("app_token")
         self.app_token_key = rospy.get_param("app_token_key")
-        self.task_finished_url = rospy.get_param("task_finished_url")
+        self.base_url = rospy.get_param("base_url")
+        self.task_finished_endpoint = rospy.get_param("task_finished_endpoint")
+        self.new_best_model_endpoint = rospy.get_param("new_best_model_endpoint")
 
-        rospy.Subscriber("scenario_finished", Bool, self.finished_task_callback)
-        rospy.Subscriber("training_finished", Empty, self.finished_task_callback)
+        rospy.Subscriber("/scenario_finished", Bool, self.finished_task_callback)
+        rospy.Subscriber("/training_finished", Empty, self.finished_task_callback)
+
+        rospy.Subscriber("/training/newBestModel", Empty, self.new_best_model_callback)
 
     def finished_task_callback(self, _):
+        self._post_request(self.task_finished_endpoint)
+
+    def new_best_model_callback(self, _):
+        self._post_request(self.new_best_model_endpoint)
+
+    def _post_request(self, endpoint):
         requests.post(
-            self.task_finished_url, 
+            os.path.join(
+                self.base_url,
+                endpoint
+            ),
             json={ "taskId": self.task_id },
-            headers={ self.app_token_key: self.app_token }
+            headers={ self.app_token_key: self.app_token }            
         )
 
 
